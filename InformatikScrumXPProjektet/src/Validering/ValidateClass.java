68a321ff3635e6b88ef8b7c1b39a249868e82fab
davidnyqvists <david.nyqvist@hotmail.com>
2016-04-22 11:50:06 +0200

    klockan 12:31

--- InformatikScrumXPProjektet/src/Validering/ValidateClass.java
+++ InformatikScrumXPProjektet/src/Validering/ValidateClass.java
@@ -11,4 +11,30 @@ package Validering;
  */
 public class ValidateClass {
     
+     //Metod 2 som kollar om man skrivit in något lösenord   
+
+    public static boolean kollaOmTomt(String inText) {
+        boolean test;
+        if (inText.isEmpty()) {
+            test = true;
+        } else {
+            test = false;
+        }
+        return test;
+    }
+    
+     //Metod 5 som kollar om längden är mindre än 30 tecken
+    public static boolean kollaLangdNamn(String inText) {
+        boolean test = false;
+        if (inText.length() < 30) {
+            test = true;
+        } else {
+            test = false;
+        }
+        return test;
+    }
+    
+    
+    
+    
 }
